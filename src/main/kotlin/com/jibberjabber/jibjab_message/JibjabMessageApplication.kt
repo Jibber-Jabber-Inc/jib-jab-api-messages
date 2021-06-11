6b17f1febba15c5fde63dbe1fada6e8f7c2c1842
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-10 21:10:26 -0300

    first commit

---
+++ src/main/kotlin/com/jibberjabber/jibjab_message/JibjabMessageApplication.kt
@@ -0,0 +1,11 @@
+package com.jibberjabber.jibjab_message
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JibjabMessageApplication
+
+fun main(args: Array<String>) {
+	runApplication<JibjabMessageApplication>(*args)
+}
---
+++ src/test/kotlin/com/jibberjabber/jibjab_message/JibjabMessageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jibberjabber.jibjab_message
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JibjabMessageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
