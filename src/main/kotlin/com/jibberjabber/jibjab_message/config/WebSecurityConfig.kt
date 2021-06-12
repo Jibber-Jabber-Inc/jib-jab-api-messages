f93c3e936c12d240ad537938846439e864d68e9d
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-12 00:41:33 -0300

    disable auth config

--- src/main/kotlin/com/jibberjabber/jibjab_message/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/jibberjabber/jibjab_message/config/WebSecurityConfig.kt
@@ -9,11 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
 
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig
 @Autowired constructor(private val unauthorizedHandler: AuthEntryPoint) : WebSecurityConfigurerAdapter() {
 
@@ -24,10 +30,28 @@ class WebSecurityConfig
 
     override
     fun configure(http: HttpSecurity) {
-        http
-            .csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
-            .headers().xssProtection().and().contentSecurityPolicy("script-src 'self'")
+        http.csrf().disable()
+        http.authorizeRequests().antMatchers("/").permitAll()
+
+//        http
+//            .csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
+//            .headers().xssProtection().and().contentSecurityPolicy("script-src 'self'")
+    }
+}
+
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+            .maxAge(MAX_AGE_SECS)
+    }
+
+    companion object {
+        private const val MAX_AGE_SECS: Long = 3600
     }
 }
