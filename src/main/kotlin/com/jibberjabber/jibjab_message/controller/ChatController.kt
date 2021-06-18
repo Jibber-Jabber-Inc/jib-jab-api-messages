0b5c3c818c225abfaf9a1fd51fea8d6129bf54df
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-18 13:55:11 -0300

    messages changes

--- src/main/kotlin/com/jibberjabber/jibjab_message/controller/ChatController.kt
+++ src/main/kotlin/com/jibberjabber/jibjab_message/controller/ChatController.kt
@@ -5,7 +5,6 @@ import com.jibberjabber.jibjab_message.dto.ChatMessageReadDto
 import com.jibberjabber.jibjab_message.factory.ChatMessageFactory
 import com.jibberjabber.jibjab_message.service.ChatMessageService
 import com.jibberjabber.jibjab_message.service.ChatRoomService
-import com.jibberjabber.jibjab_message.utils.SessionUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.messaging.handler.annotation.MessageMapping
@@ -21,7 +20,6 @@ class ChatController @Autowired constructor(
     val messagingTemplate: SimpMessagingTemplate,
     val chatMessageService: ChatMessageService,
     val chatRoomService: ChatRoomService,
-    val sessionUtils: SessionUtils,
     val chatMessageFactory: ChatMessageFactory
 ) {
 
@@ -36,23 +34,20 @@ class ChatController @Autowired constructor(
     @MessageMapping("/read")
     fun processReadChat(@Payload chatMessageRead: ChatMessageReadDto) {
         val chatMessage = chatMessageService.getChatMessage(chatMessageRead.messageId)
-        print("chatMessage $chatMessage")
         chatMessageService.markMessageAsRead(chatMessage.id!!)
         val saved = chatMessageService.getChatMessage(chatMessageRead.messageId)
         messagingTemplate.convertAndSendToUser(chatMessage.recipientId, "/queue/read", saved)
         messagingTemplate.convertAndSendToUser(chatMessage.senderId, "/queue/read", saved)
     }
 
-    @GetMapping("/api/message/messages/{userId}/count")
-    fun countNewMessages(@PathVariable userId: String): ResponseEntity<Long> {
-        val user = sessionUtils.getTokenUserInformation()
-        return ResponseEntity.ok(chatMessageService.countNewMessages(userId, user.id))
+    @GetMapping("/api/message/messages/{userId}/{loggedId}/count")
+    fun countNewMessages(@PathVariable userId: String, @PathVariable loggedId: String): ResponseEntity<Long> {
+        return ResponseEntity.ok(chatMessageService.countNewMessages(userId, loggedId))
     }
 
-    @GetMapping("/api/message/messages/{userId}")
-    fun findChatMessages(@PathVariable userId: String): ResponseEntity<*> {
-        val user = sessionUtils.getTokenUserInformation()
-        return ResponseEntity.ok(chatMessageService.findChatMessages(userId, user.id).sortedBy { it.timestamp })
+    @GetMapping("/api/message/messages/{userId}/{loggedId}")
+    fun findChatMessages(@PathVariable userId: String , @PathVariable loggedId: String): ResponseEntity<*> {
+        return ResponseEntity.ok(chatMessageService.findChatMessages(userId, loggedId).sortedBy { it.timestamp })
     }
 
 //    @GetMapping("/messages/{id}")
