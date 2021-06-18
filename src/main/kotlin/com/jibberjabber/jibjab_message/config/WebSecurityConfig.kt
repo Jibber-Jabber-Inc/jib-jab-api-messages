2434da192d8d5069556c87fdbd1f7b66031cec9b
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-18 12:39:34 -0300

    reviewing message service auth in websocket

--- src/main/kotlin/com/jibberjabber/jibjab_message/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/jibberjabber/jibjab_message/config/WebSecurityConfig.kt
@@ -33,7 +33,7 @@ class WebSecurityConfig
         http
             .csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .authorizeRequests().antMatchers("/ws/**").permitAll()
+            .authorizeRequests().antMatchers("/ws/*").permitAll()
             // SWAGGER CONFIG
             .antMatchers("/v3/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
             .anyRequest().authenticated().and()
