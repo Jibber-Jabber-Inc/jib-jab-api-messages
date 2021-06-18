4318d3bbce604544530bcf5456be790f83b7a37d
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-17 21:00:51 -0300

    added modifying and transactional to query

--- src/main/kotlin/com/jibberjabber/jibjab_message/repository/ChatMessageRepository.kt
+++ src/main/kotlin/com/jibberjabber/jibjab_message/repository/ChatMessageRepository.kt
@@ -3,12 +3,16 @@ package com.jibberjabber.jibjab_message.repository
 import com.jibberjabber.jibjab_message.domain.ChatMessage
 import com.jibberjabber.jibjab_message.domain.MessageStatus
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import javax.transaction.Transactional
 
 interface ChatMessageRepository : JpaRepository<ChatMessage, String> {
     fun countBySenderIdAndRecipientIdAndStatus(senderId: String, recipientId: String, status: MessageStatus): Long
     fun findByChatId(chatId: String): List<ChatMessage>
 
     @Query("update ChatMessage set status = ?3 where senderId = ?1 and recipientId = ?2")
+    @Modifying
+    @Transactional
     fun updateStatuses(senderId: String, recipientId: String, delivered: MessageStatus)
 }
