d071fb7cf9921b5455ced42a5397b67079af9432
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-25 00:05:46 -0300

    changes ws https

--- src/main/kotlin/com/jibberjabber/jibjab_message/config/WebSocketConfig.kt
+++ src/main/kotlin/com/jibberjabber/jibjab_message/config/WebSocketConfig.kt
@@ -16,7 +16,7 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     private val clientUrl: String = "http://localhost:3000"
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/ws").withSockJS()
+        registry.addEndpoint("/ws").setAllowedOrigins("http://revproxy").withSockJS()
     }
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
